76245aed9ba39f4acb1d61103797d02ef67a285b
gonz410s <gonzalitoxtm@gmail.com>
2024-01-11 16:37:02 -0300

    tengo casi todo hecho

--- main.js
+++ main.js
@@ -19,20 +19,54 @@ function mostrarOpciones() {
 }
 
 mostrarOpciones();
+function formularioIngreso() {
+    let nombre = prompt("Como te llamas?:");
+    let edad = prompt("Cuantas vueltas al sol tenes?:");
+    let email = prompt("Por favor dejame un email:");
 
-    function formularioIngreso() {
-        let nombre = prompt("Como te llamas?:");
-        let edad = prompt("Cuantas vueltas al sol tenes?:");
-        let email = prompt("Por favor dejame un email:");
-        
-        if ((nombre !="" && nombre != Number)&&(edad !="" && edad != String)&&(email !="")){
-            alert("Aqui estan tus datos =) :\nNombre: " + nombre + "\nEdad: " + edad + "\nEmail: " + email);}
-            else{
-                alert("Error, por favor ingresa alguno de los datos que te faltan");
-            formularioIngreso();
-        }
-        }
+    console.log(nombre, edad, email);
+    if ((nombre !== "" && nombre !== Number) && (edad !== "" && edad !== String) && (email !== "")) {
+        alert("Aqui estan tus datos =) :\nNombre: " + nombre + "\nEdad: " + edad + "\nEmail: " + email);
+        mostrarOpciones();
+    }
+    else {
+        alert("Error, por favor ingresa alguno de los datos que te faltan");
+        formularioIngreso();
+    }
+}
 
-        
+function formularioIngreso2() {
+    let entretenimiento = prompt("De estas opciones, a cual le dedicas mas horas de ocio: \nCine \nSeries \nAnime \nGaming \nMusica");
+    if (
+        entretenimiento !== "" && 
+        entretenimiento === "cine" || 
+        entretenimiento === "musica" || 
+        entretenimiento === "series" || 
+        entretenimiento === "anime" || 
+        entretenimiento === "gaming")
+    {
+        let deporte = prompt("Cual es tu deporte favorito?:");
+        if(deporte=="futbol"){
+            alert("Ahhhh Futbolero saliste ehhh")
+        }
+        let estudioTrabajo = prompt("Estas estudiando o trabajando actualmente, puede ser nada tambien:");
+            if(
+                estudioTrabajo!=="estudio"&&
+                estudioTrabajo!=="trabajando"&&
+                estudioTrabajo!=="trabajo"&& 
+                estudioTrabajo!=="estudiando"){
+                alert("Es hora de pensar en hacer algo con ese cuerpo")
+            }
+        alert("Por lo que me contaste, estos son tus gustos :\nLo que mas horas metes cuando estas libre: " + entretenimiento + "\nTe gusta mucho el: " + deporte + "\nActualmente estas ocupado en: " + estudioTrabajo);
+        console.log(entretenimiento, deporte, estudioTrabajo)
+    }
+    else{
+        alert("Tu hobby no es el correcto, debes ingresar una de las opciones dadas.");
+        formularioIngreso2();
+    }
     
+    
+}
+
+
 
